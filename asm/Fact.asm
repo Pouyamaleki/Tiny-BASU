33179d765cdab39ee32dacc42fbaab786e58e5da
Pouya <pouyamaleki85@gmail.com>
2026-06-26 15:26:21 +0330

    adjust the indentation for the comments

--- asm/Fact.asm
+++ asm/Fact.asm
@@ -1,17 +1,17 @@
-li rx1, 50                    # rx1 = 50
-li rx2, 1                     # rx2 = 1
+li rx1, 50                           # rx1 = 50
+li rx2, 1                            # rx2 = 1
 
-factorial_loop:               # Factorial loop label
-    li rx3, 0                 # rx3 = 0
-    li rx4, 0                 # rx4 = 0
+factorial_loop:                      # Factorial loop label
+    li rx3, 0                        # rx3 = 0
+    li rx4, 0                        # rx4 = 0
     
-multiply_loop:                #multiply loop label
-    add rx3, rx3, rx2         # rx3 = rx3 + rx2
-    addi rx4, rx4, 1          # rx4 = rx4 + 1
-    bne rx4, rx1, multiply_loop  # if rx4 != rx1 go back to the multiply loop
+multiply_loop:                       # multiply loop label
+    add rx3, rx3, rx2                # rx3 = rx3 + rx2
+    addi rx4, rx4, 1                 # rx4 = rx4 + 1
+    bne rx4, rx1, multiply_loop      # if rx4 != rx1 go back to the multiply loop
     
-    addi rx2, rx3, 0          # rx2 = rx3
-    addi rx1, rx1, -1         # rx1 = rx1 - 1
-    bne rx1, rx0, factorial_loop  # if rx1 != rx0 go back to the first loop
+    addi rx2, rx3, 0                 # rx2 = rx3
+    addi rx1, rx1, -1                # rx1 = rx1 - 1
+    bne rx1, rx0, factorial_loop     # if rx1 != rx0 go back to the first loop
 
 # when the loop finish rx2 is equal to 50
