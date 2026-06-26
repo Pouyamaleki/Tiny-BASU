e3c38693d7f7bf0ed9bcf2ed0044fe4a4a4cf5da
Pouya <pouyamaleki85@gmail.com>
2026-06-26 15:24:57 +0330

    add comments to the code

--- asm/Fact.asm
+++ asm/Fact.asm
@@ -1,18 +1,17 @@
-li rx1, 50                    # rx1 = 50 (عدد اولیه برای فاکتوریل)
-li rx2, 1                     # rx2 = 1 (حاصل‌ضرب)
+li rx1, 50                    # rx1 = 50
+li rx2, 1                     # rx2 = 1
 
-factorial_loop:
-    # rx2 = rx2 × rx1 رو با جمع‌های مکرر انجام بده
-    li rx3, 0                 # rx3 = 0 (مقدار اولیه برای جمع)
-    li rx4, 0                 # rx4 = 0 (شمارنده برای جمع)
+factorial_loop:               # Factorial loop label
+    li rx3, 0                 # rx3 = 0
+    li rx4, 0                 # rx4 = 0
     
-multiply_loop:
+multiply_loop:                #multiply loop label
     add rx3, rx3, rx2         # rx3 = rx3 + rx2
     addi rx4, rx4, 1          # rx4 = rx4 + 1
-    bne rx4, rx1, multiply_loop  # اگه rx4 != rx1، برگرد به جمع
+    bne rx4, rx1, multiply_loop  # if rx4 != rx1 go back to the multiply loop
     
-    addi rx2, rx3, 0          # rx2 = rx3 (نتیجه ضرب رو بذار توی rx2)
+    addi rx2, rx3, 0          # rx2 = rx3
     addi rx1, rx1, -1         # rx1 = rx1 - 1
-    bne rx1, rx0, factorial_loop  # اگه rx1 != 0، برگرد به اول حلقه
+    bne rx1, rx0, factorial_loop  # if rx1 != rx0 go back to the first loop
 
-# بعد از اتمام حلقه، rx2 = 50! هست
+# when the loop finish rx2 is equal to 50
