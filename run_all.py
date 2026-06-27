1f199423fd4b8e7794779ddd152ab41d3986d9ae
Pouya <pouyamaleki85@gmail.com>
2026-06-28 01:25:29 +0330

    fix the run_all.py bugs

--- run_all.py
+++ run_all.py
@@ -39,8 +39,8 @@ REPORTS_FOLDER = os.path.join(BASE_DIR, "Reports")
 programs = [
     ("fibonacci_bne.asm", "fibo_bne"),
     ("fibonacci_beq.asm", "fibo_beq"),
-    ("fact.asm", "fact")
-    ("new_instructions.asm", "new_instructions")
+    ("fact.asm", "fact"),
+    ("misc/new_instructions.asm", "new_instructions")
 ]
 
 methods = ["ST", "SN", "D1", "D2", "IQ"]
