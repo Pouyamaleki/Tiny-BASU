1919c92d2a4e1a239753d364c7a55bc828cb3832
Pouya <pouyamaleki85@gmail.com>
2026-06-28 00:35:05 +0330

    fix the indentation in comment section

--- misc/new_instructions.asm
+++ misc/new_instructions.asm
@@ -6,14 +6,14 @@ li rx2, 4                        # rx2 = 4
 mul rx3, rx1, rx2                # rx3 = 6 * 4 = 24
 div rx4, rx3, rx2                # rx4 = 24 / 4 = 6
 
-sll rx5, rx1, rx2                # rx5 = 6 << 4 = 96 (<< means shift to left)
-srl rx5, rx5, rx2                # rx5 = 96 >> 4 = 6 (>> means shift to right)
+sll rx5, rx1, rx2                # rx5 = 6 << 4 = 96    ( << means shift to left)
+srl rx5, rx5, rx2                # rx5 = 96 >> 4 = 6    ( >> means shift to right)
 
 slli rx0, rx1, 2                 # rx0 = 6 << 2 = 24
 srli rx0, rx0, 1                 # rx0 = 24 >> 1 = 12
 
-andi rx0, rx3, 15                # rx0 = 24 & 15 = 8 (& is the and sign)
-ori rx0, rx0, rx1                # rx0 = 8 | 6 = 14 (| is the or sign)
+andi rx0, rx3, 15                # rx0 = 24 & 15 = 8 ( & is the and sign)
+ori rx0, rx0, rx1                # rx0 = 8 | 6 = 14 ( | is the or sign)
 
 li rx7, 10                       # rx7 = 10
 li rx6, 0                        # rx6 = 0
@@ -21,12 +21,12 @@ li rx6, 0                        # rx6 = 0
 sum_loop:                        # loop label
     add rx6, rx6, rx7            # rx6 = rx6 + rx7 (this is an accumulated sum)
     addi rx7, rx7, -1            # rx7 = rx7 - 1
-    bgtz rx7, sum_loop           # if rx7 > 0 go to sum_loop
+    bgtz rx7, sum_loop           # if (rx7 > 0) go to sum_loop
 
 check_neg:                       # check negative number label
     li rx1, 3                    # rx1 = 3
     addi rx1, rx1, -5            # rx1 = 3 - 5 = -2 (negative)
-    bltz rx1, done               # if rx1 < 0 jump to done
+    bltz rx1, done               # if (rx1 < 0) jump to done
 
 done:                            # end label
     addi rx5, rx6, 0             # rx5 = rx6 = 55 (final result)
